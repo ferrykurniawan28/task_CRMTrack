3345d05a5cabd30d3070096060f5305c9fdf4ffc
Faiz Abdurrahman <mufaman.27@gmail.com>
2025-02-21 13:44:30 +0700

    [UPDATE] home screen

--- lib/ui/widgets/home_list_card.dart
+++ lib/ui/widgets/home_list_card.dart
@@ -1,11 +1,12 @@
+import 'package:crm_track/helpers/helpers.dart';
 import 'package:flutter/material.dart';
 
-class HomeListCard extends StatelessWidget {
+class HomeListCard extends StatefulWidget {
   final String type;
   final String title;
-  final bool isCompleted;
+  bool isCompleted;
   final String time;
-  const HomeListCard({
+  HomeListCard({
     super.key,
     required this.type,
     required this.title,
@@ -14,41 +15,55 @@ class HomeListCard extends StatelessWidget {
   });
 
   @override
+  State<HomeListCard> createState() => _HomeListCardState();
+}
+
+class _HomeListCardState extends State<HomeListCard> {
+  @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+      margin: EdgeInsets.only(bottom: 10),
+      width: double.infinity,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(
-          Radius.circular(20),
+          Radius.circular(8),
         ),
         color: Color.fromRGBO(249, 250, 251, 1),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Row(
             children: [
               Container(
                 padding: EdgeInsets.all(8),
-                child: type == 'Meeting'
+                child: widget.type == 'Meeting'
                     ? Image.asset(
                         'assets/icon/telephone.png',
+                        height: 24,
+                        width: 24,
                       )
                     : Image.asset(
                         'assets/icon/chat-quote.png',
+                        height: 24,
+                        width: 24,
                       ),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+                    Radius.circular(20),
                   ),
-                  color: type == 'Meeting'
+                  color: widget.type == 'Meeting'
                       ? Color.fromRGBO(241, 69, 72, 1)
                       : Color.fromRGBO(1, 186, 239, 1),
                 ),
               ),
+              spacerWidth(12),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    title,
+                    widget.title,
                     style: TextStyle(
                       fontSize: 14,
                       fontWeight: FontWeight.w500,
@@ -56,7 +71,7 @@ class HomeListCard extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    time,
+                    widget.time,
                     style: TextStyle(
                       fontSize: 12,
                       fontWeight: FontWeight.w400,
@@ -66,7 +81,39 @@ class HomeListCard extends StatelessWidget {
                 ],
               ),
             ],
-          )
+          ),
+          Container(
+            child: widget.isCompleted
+                ? GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        widget.isCompleted = !widget.isCompleted;
+                      });
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        color: Color.fromRGBO(201, 233, 218, 1),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                      child: Text(
+                        'Completed',
+                        style: TextStyle(
+                            color: Color.fromRGBO(25, 135, 84, 1),
+                            fontSize: 10,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  )
+                : Checkbox(
+                    value: widget.isCompleted,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        widget.isCompleted = value!;
+                      });
+                    },
+                  ),
+          ),
         ],
       ),
     );
--- lib/ui/widgets/home_task_list_widget.dart
+++ lib/ui/widgets/home_task_list_widget.dart
@@ -1,9 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
-import 'package:crm_track/cubit/task/task_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
+import 'package:crm_track/cubit/task/task_cubit.dart';
 import 'home_list_card.dart';
 
 class HomeTaskListWidget extends StatefulWidget {
@@ -15,29 +12,40 @@ class HomeTaskListWidget extends StatefulWidget {
 
 class _HomeTaskListWidgetState extends State<HomeTaskListWidget> {
   @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<TaskCubit>().getTasks();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Positioned(
+          left: 0,
+          right: 0,
           child: Container(
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(20),
-              ),
+              borderRadius: BorderRadius.all(Radius.circular(20)),
               color: Colors.red,
             ),
             height: 40,
           ),
         ),
         Positioned(
-          width: MediaQuery.of(context).size.width,
           top: 2,
+          left: 0,
+          right: 0,
           child: Container(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-              color: Color.fromRGBO(249, 250, 251, 1),
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+              color: Colors.white,
             ),
             child: Column(
               children: [
@@ -80,69 +88,56 @@ class _HomeTaskListWidgetState extends State<HomeTaskListWidget> {
                           ),
                         ],
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            'View all tasks',
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w500,
-                            ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'View all tasks',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w500,
                           ),
-                        ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-                BlocBuilder<TaskCubit, TaskState>(
-                  builder: (context, state) {
-                    if (state is TaskLoading) {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                    if (state is TaskError) {
-                      return Center(
-                        child: Text(state.message),
-                      );
-                    }
-                    if (state is TaskLoaded) {
-                      final data = state.tasks;
-                      if (data.isEmpty) {
-                        return Center(
-                          child: Text('No tasks'),
+                SizedBox(
+                  height: 300, // Pastikan ukuran tetap
+                  child: BlocBuilder<TaskCubit, TaskState>(
+                    builder: (context, state) {
+                      if (state is TaskLoading) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      if (state is TaskError) {
+                        return Center(child: Text(state.message));
+                      }
+                      if (state is TaskLoaded) {
+                        final data = state.tasks;
+                        if (data.isEmpty) {
+                          return Center(child: Text('No tasks'));
+                        }
+                        return ListView.builder(
+                          itemCount: data.length,
+                          itemBuilder: (context, index) {
+                            final dataTask = data[index];
+                            return HomeListCard(
+                              isCompleted: dataTask.isCompleted,
+                              time: dataTask.time,
+                              title: dataTask.title,
+                              type: dataTask.type,
+                            );
+                          },
                         );
                       }
-                      return Container(
-                        child: Column(
-                          children: [
-                            ListView.builder(
-                              itemCount: data.length,
-                              itemBuilder: (context, index) {
-                                final dataTask = data[index];
-                                return HomeListCard(
-                                  isCompleted: dataTask.isCompleted,
-                                  time: dataTask.time,
-                                  title: dataTask.title,
-                                  type: dataTask.type,
-                                );
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    }
-                    return Center(
-                      child: Text('unvailable state'),
-                    );
-                  },
+                      return Center(child: Text('Unavailable state'));
+                    },
+                  ),
                 ),
               ],
             ),
           ),
-        )
+        ),
       ],
     );
   }
